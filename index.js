5f6f4470a3d500d1950dc64797b898055471e466
GeryMolina <g.molina.ojeda@gmail.com>
2019-10-02 14:36:45 -0300

    JS preguntas de years

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const actores = []
 const lista= document.getElementById('peliculas');
 //Actores
-const avengers = () => {
+const actorsAvengers = () => {
 fetch('http://www.omdbapi.com/?apikey=3a4ab00&t=the-Avengers&plot=full')
  .then(res => res.json())
  .then(movies => { 
@@ -39,9 +39,7 @@ fetch('http://www.omdbapi.com/?apikey=3a4ab00&t=the-Avengers&plot=full')
             <input type="radio"> ${actor}<br>
             <input type="radio"> Meryl Streep, Anne Hathaway, Emily Blunt, Mark Ruffalo
             </p>    
-            `
-    
-        //movies.Search.map(x => console.log(x.Title))   
+            `  
         })
      .catch(error => error);
 }
@@ -52,26 +50,112 @@ const movieActors = () => {
      .then(movies => { 
         const actor= movies.Actors;
         const imageMovie= movies.Poster;
-        console.log(movies)
             lista.innerHTML= 
              `
             <p>¿Quienes protagonizan la pelicula Indiana Jones? 
                 <div> <img src= "${imageMovie}"> </div>
-            <p><input type="radio"> Mel Gibson, Sean Connery, Denholm Elliott, Alison Doody"<br>
-            <input type="radio"> Richard Gere, Sean Connery, Denholm Elliott, Alison Doody"<br>
-            <input type="radio"> Jhon Travolta, Sean Connery, Denholm Elliott, Alison Doody" <br>
+            <p><input type="radio"> Mel Gibson, Sean Connery, Denholm Elliott, Alison Doody<br>
+            <input type="radio"> Richard Gere, Sean Connery, Denholm Elliott, Alison Doody<br>
+            <input type="radio"> Jhon Travolta, Sean Connery, Denholm Elliott, Alison Doody<br>
             <input type="radio"> ${actor}
             </p>    
             `
-    
-        //movies.Search.map(x => console.log(x.Title))   
         })
      .catch(error => error);
      }
 
- const boton = document.getElementById('boton');
- boton.addEventListener('click',() => {
-    avengers()
-    movieActress();
-    movieActors();
-})
+const boton = document.getElementById('boton');
+boton.addEventListener('click',() => {
+     //actorsAvengers()
+     //movieActress();
+    //movieActors();
+    //yearsAvengers();
+    //yearsWizard();
+    //yearsTitanic();
+    countryAmelie();
+    })
+    
+// Años de Estreno
+
+const yearsAvengers = () => {
+    fetch('http://www.omdbapi.com/?apikey=3a4ab00&t=the-Avengers&plot=full')
+     .then(res => res.json())
+     .then(movies => { 
+        const years= movies.Year;
+        const imageMovie= movies.Poster;
+            lista.innerHTML= 
+             `
+            <p>¿En qué año se estrenó la pelicula The Avengers? 
+                <div> <img src= "${imageMovie}"> </div>
+            <p><input type="radio"> 1998<br>
+            <input type="radio"> 2000 <br>
+            <input type="radio"> ${years}<br>
+            <input type="radio"> 1995
+            </p>    
+            `
+        })
+     .catch(error => error);
+     }
+
+const yearsWizard = () => {
+    fetch('http://www.omdbapi.com/?apikey=3a4ab00&t=the-Wizard-Of-Oz&plot=full')
+    .then(res => res.json())
+    .then(movies => { 
+        const years= movies.Year;
+        const imageMovie= movies.Poster;
+            lista.innerHTML= 
+                 `
+            <p>¿Cúal es el año de estreno de el Mago de Oz? 
+                <div> <img src= "${imageMovie}"></div>
+                <p><input type="radio"> ${years}<br>
+                <input type="radio"> 2009<br>
+                <input type="radio"> 2011<br>
+                <input type="radio"> 2013
+            </p>    
+                `
+     })
+         .catch(error => error);
+}
+
+
+const yearsTitanic = () => {
+    fetch('http://www.omdbapi.com/?apikey=3a4ab00&t=titanic&plot=full')
+    .then(res => res.json())
+    .then(movies => { 
+        const years= movies.Year;
+        const imageMovie= movies.Poster;
+            lista.innerHTML= 
+                 `
+            <p>¿Qué año se estrenó Titanic? 
+                <div> <img src= "${imageMovie}"></div>
+                <p><input type="radio"> ${years}<br>
+                <input type="radio"> 2009 <br>
+                <input type="radio"> 2011<br>
+                <input type="radio"> 2013
+            </p>    
+                `
+     })
+         .catch(error => error);
+}
+// Pais de origen
+const countryAmelie = () => {
+    fetch('http://www.omdbapi.com/?apikey=3a4ab00&t=Amélie&plot=full')
+    .then(res => res.json())
+    .then(movies => { 
+        const years= movies;
+        console.log(movies)
+        const imageMovie= movies.Poster;
+            lista.innerHTML= 
+                 `
+            <p>¿Qué año se estrenó Titanic? 
+                <div> <img src= "${imageMovie}"></div>
+                <p><input type="radio"> ${years}<br>
+                <input type="radio"> 2009 <br>
+                <input type="radio"> 2011<br>
+                <input type="radio"> 2013
+            </p>    
+                `
+    })
+    .catch(error => error);
+}
+
